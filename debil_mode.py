b97377a184af937e11275316247cf9399db62a95
JILonopa3pblBaTeJlb <129288487+JILonopa3pblBaTeJlb@users.noreply.github.com>
2024-01-28 21:53:34 +0200

    Add files via upload
    Исправлен режим долбоеба в сторону баланса читаемости и естественности

--- debil_mode.py
+++ debil_mode.py
@@ -3,29 +3,28 @@ import re
 
 def handle_debil_mode(text, context):
     if context.bot_data.get('debil_mode'):
-        text = re.sub(r'[^\w\s]', '', text).lower()
-        text = text.replace('\n', ' ').replace('\r', '')
-
-        # Удаление каждого 10-го знака с вероятностью 0.1 для каждого символа
-        text = ''.join(char for i, char in enumerate(text) if (i + 1) % 10 != 0 or random.random() > 0.1)
+        text = re.sub(r'[^\w\s]', '', text).lower().replace('\n', ' ').replace('\r', ' ')
         
-        # Замена символов а=о, о=а, и=е, е=и с вероятностью 0.01 для каждого символа
         char_map = {'а': 'о', 'о': 'а', 'и': 'е', 'е': 'и'}
-        num_replacements = int(len(text) * 0.1)
-        replace_indices = random.sample(range(len(text)), num_replacements)
-        text = ''.join(char_map[char] if i in replace_indices and char in char_map else char for i, char in enumerate(text))
-        
-        # Безусловная замена "ться" на "ца" и "тся" на "ццо"
-        text = text.replace('ться', 'ца').replace('тся', 'ццо')
-        
-        # Удаление пробелов с вероятностью 0.1
-        text = ''.join(char if char != ' ' or random.random() > 0.05 else '' for char in text)
-        
-        additional_words = ["нахуй", "сукбля", "сцук", "бля", "бля нахуй ебать", "епта", "епты", "епт"]
         words = text.split()
-        for i in range(4, len(words), 6):
-            insert_word = random.choice(additional_words)
-            words.insert(i, insert_word)
-        text = ' '.join(words)
+        text = ' '.join(''.join(char_map[char] if char in char_map and random.random() < 0.05 else char for char in word) if len(word) > 4 else word for word in words)
+
+        words = text.split()
+        text = ' '.join(''.join(char for i, char in enumerate(word) if (i + 1) % 5 != 0 or random.random() > 0.02) if len(word) > 3 else word for word in words)
+        
+        text = text.replace('ться', 'ца').replace('тся', 'ццо')
+        text = ''.join(char if char != ' ' or random.random() > 0.02 else '' for char in text)
+        
+        char_replacement_map = {'а': 'ва', 'ы': 'ыф', 'ч': 'чс', 'р': 'пр', 'д': 'дж', 'т': 'ть'}
+        text = ''.join(char_replacement_map[char] if char in char_replacement_map and random.random() < 0.01 else char for char in text)
+        
+        words = text.split()
+        new_words = []
+        for i, word in enumerate(words):
+            new_words.append(word)
+            if (i + 1) % 3 == 0 and len(word) > 3:
+                new_words.append(random.choice(["нахуй", "сукбля", "сцук", "бля", "бля нахуй ебать", "епта", "епты", "епт"]))
+        
+        text = ' '.join(new_words) + ' ' + ' '.join(random.sample(["ебать", "сук", "бля", "нахуй", "псдц"], k=3))
 
     return text
